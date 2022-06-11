a746e41bc23ef5a54f9a07b024bd3a8593796628
benjytan45678 <Benjytan45678@gmail.com>
2022-06-11 15:20:16 +0800

    current-location

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 import HomeScreen from './app/screens/HomeScreen';
 import RegistrationPage from './app/screens/RegistrationPage';
 
+import Map from './app/screens/Map';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,6 +19,7 @@ const App = () => {
         <Stack.Screen options = { {headerShown: false}} name ="tabs" component={Tabs} /> 
       </Stack.Navigator>
     </NavigationContainer>
+  
   );
 }
 
---
+++ app/screens/Map.js
@@ -0,0 +1,43 @@
+import React,{useEffect,useState} from 'react'
+import MapView from 'react-native-maps'
+import * as Location from "expo-location"
+import {View,Text,TouchableOpacity, Stylesheet} from "react-native"
+
+const Map = () => {
+  const [startLocation,setstartLocation] = useState('');
+  
+  useEffect(() => {
+    async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied'); // testing
+        return;
+      }
+      let newLocation = await Location.getCurrentPositionAsync({});
+      setstartLocation({
+        region: {
+         latitude: location.coords.latitude,
+         longitude: location.coords.longitude,
+         latitudeDelta: 0.0922,
+         longitudeDelta: 0.0421,
+        },
+       });
+    };
+  }, []);
+
+
+  return (
+    <MapView
+        style={{ alignSelf: 'stretch', height: '100%' }}
+        region={location}
+      >
+    <Marker coordinate={location} title='Marker' >
+    <Callout> 
+    <Text> Starting Point </Text>
+    </Callout>
+    </Marker>
+    </MapView>
+  )
+}
+
+export default Map
