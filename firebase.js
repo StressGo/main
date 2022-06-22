914794f0cca63af8f4659af0b286054469544d14
hongsheng <loy.hong.shenggg@gmail.com>
2022-06-23 00:59:21 +0800

    firebase

--- firebase.js
+++ firebase.js
@@ -1,31 +1,44 @@
 
 // import all functions needed from the SDKS needed
+import firebase from 'firebase/app'
 import {initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
-
-const FIREBASE_APIKEY="AIzaSyD1Hk1asVux06fU55J3FaaYfyIUI4sM1B4"
-const FIREBASE_AUTHDOMAIN="stressgo-3cef0.firebaseapp.com"
-const FIREBASE_PROJECTID="stressgo-3cef0"
-const FIREBASE_STORAGEBUCKET="stressgo-3cef0.appspot.com"
-const FIREBASE_MESSAGINGSENDERID="316778582160"
-const FIREBASE_APPID="1:316778582160:web:3502f9529218e8fc838815"
-const FIREBASE_MEASUREMENTID="G-BLT8YK6SMG"
+import { getDocs, getFireStore, collection } from 'firebase/firestore'
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: FIREBASE_APIKEY,
-    authDomain: FIREBASE_AUTHDOMAIN,
-    projectId: FIREBASE_PROJECTID,
-    storageBucket: FIREBASE_STORAGEBUCKET,
-    messagingSenderId: FIREBASE_MESSAGINGSENDERID,
-    appId: FIREBASE_APPID,
-    measurementId: FIREBASE_MEASUREMENTID,
-  }
+  apiKey: "AIzaSyD1Hk1asVux06fU55J3FaaYfyIUI4sM1B4",
+  authDomain: "stressgo-3cef0.firebaseapp.com",
+  databaseURL: "https://stressgo-3cef0-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "stressgo-3cef0",
+  storageBucket: "stressgo-3cef0.appspot.com",
+  messagingSenderId: "316778582160",
+  appId: "1:316778582160:web:3502f9529218e8fc838815",
+  measurementId: "G-BLT8YK6SMG"
+};
 
 
 
+  // init firebase app
   const app = initializeApp(firebaseConfig)
+
+  // init services
+  const db = getFireStore()
+
+  // collection ref
+  const colRef = collection(db, 'events')
+
+  // get collection data
+  getDocs(colRef).then((snapshot) => {
+    let events = []
+    snapshot.docs.forEach((doc) => {
+      events.push({ ...doc.data(), id: doc.id })
+    })
+  })
+  .catch(err => {
+    console.log(err.message)
+  })
+
   export const auth = getAuth(app);
 
 
